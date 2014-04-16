61b4ed42ae7dddfaf6365f8c20f8d4d1cd155b19
Simon Eriksson <k.simon.eriksson@gmail.com>
2014-04-16 20:06:25 +0200

    Cleaned up command line args

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 var yargs = require('yargs')
       .usage('Translate from tyda.se. \n Usage: $0 [text]')
       .example('$0 hello', 'Translate hello to Swedish')
-      .example('$0 scharwz -l de', 'Translate scharwz to Swedish')
-      .default('l', 'en')
-      .default('t', 'sv')
-      .alias('l', 'lang')
-      .alias('h', 'help')
-      .demand(['l','t']);
+      .example('$0 schwarz -t de', 'Translate schwarz to Swedish')
+      .default('t', 'en')
+      .alias('t', 'to')
+      .alias('h', 'help');
+
 var tyda = require('./tyda');
 
 if(yargs.argv.help){
@@ -22,8 +21,7 @@ if(yargs.argv._.length <= 0){
   return;
 }
 
-
-tyda.translate(yargs.argv._[0], yargs.argv.t, yargs.argv.l, function(err, res){
+tyda.translate(yargs.argv._[0], 'sv', yargs.argv.to, function(err, res){
   if(err){
     return console.error(err);
   }
